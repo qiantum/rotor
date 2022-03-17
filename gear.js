fc57915ff582c318e75ce899028656cd05b91880
cto <cto@zt>
2022-03-17 15:13:37 +0800

    齿轮中心距、啮合半径

--- gear.js
+++ gear.js
@@ -2,7 +2,7 @@
 // License: GPL v3
 
 // 渐开线齿轮 Involute Gear
-function GearI({
+function GearInv({
 	M, // 模数
 	A = 20, // 压力角度数
 	Z, // 齿数
@@ -15,16 +15,15 @@ function GearI({
 	if ((M = round(M * 8) >> 3) <= 0) throw 'err M'
 	if (!((A = roundepsi(A)) > 0)) throw 'err A'
 	if (Z != (Z |= 0) || Z < 6) throw 'err Z'
-	if (!((S = roundepsi(S)) >= 0)) throw 'err S'
 	tickn = max(ceil(tickn / Z / 2), 3) * Z * 2 // 齿廓步进数，齿数整倍数
 
-	let TA = (A * PI) / 180 // 压力角
+	let AA = (A * PI) / 180 // 压力角
 	let TP = PI2 / Z // 齿距角
 	let TQ = PI / Z // 齿顶根角
-	let TB = (PI + 4 * S * tan(TA)) / Z + (tan(TA) - TA) * 2 // 基角（依据基点，齿厚角+两侧分度基角）
+	let TB = (PI + 4 * S * tan(AA)) / Z + GearInv.T(AA) * 2 // 基角（依据基点，齿厚角+两侧分度展角）
 
 	let P = M * Z * 0.5 // 分度圆半径
-	let B = P * cos(TA) // 基圆半径
+	let B = P * cos(AA) // 基圆半径
 	let U = P + M * (S + (I ? 1.25 : 1)) // 齿顶半径
 	let F = P + M * (S - (I ? 1 : 1.25)) // 齿根半径
 	if (I && F < B) F = B
@@ -32,7 +31,7 @@ function GearI({
 	// U = Q + M * (I ? 1.25 : 1) // 齿顶半径
 	// F = Q - M * (I ? 1 : 1.25) // 齿根半径
 	// let CQ = sqrt(Q * Q - B * B) / B // 中分渐开角
-	// TB = TQ + (CQ - acos(B / Q)) * 2 // 基角（依据基点，中分齿厚角+两侧中分基角）
+	// TB = TQ + (CQ - acos(B / Q)) * 2 // 基角（依据基点，中分齿厚角+两侧中分展角）
 
 	let CU = sqrt(U * U - B * B) / B // 齿顶渐开角
 	let CF = sqrt(max(F * F - B * B, 0)) / B // 齿根渐开角
@@ -42,16 +41,16 @@ function GearI({
 	let TZ = z => (z * PI2) / Z - TB / 2 // 齿起始角（依据基点）
 
 	let T2 = (T, gear2) => (T / gear2.Z) * Z * (I == gear2.I ? -1 : 1)
-	let E = gear2 => abs(P + gear2.P * (I == gear2.I ? -1 : 1))
+	let G2 = (E, gear2) => (E * Z) / max(abs(Z + gear2.Z * (I == gear2.I ? 1 : -1)), 1)
 
 	zoom ??= ceil(+size || min(size.width, size.height)) / (B * 2 + M * 3)
 	size = ceil(U * zoom * 2)
 	Object.assign(this, { zoom, size, M, A, Z, S, I, B, P, U, F })
-	Object.assign(this, { TP, TQ, TZ, T2, E })
+	Object.assign(this, { TP, TQ, TZ, T2, G2 })
 
 	// 参数显示
 	function params(T) {
-		return _`M${M} A${A}__Z${Z} S${S}__B${B}{1} P${P}{1}__F${F}{1} U${U}{1}__`
+		return _`M${M} A${A}__Z${Z} S${S}{2}__B${B}{.1} P${P}{.1}__F${F}{.1} U${U}{.1}__`
 	}
 	console.log(...params().split('__'), _`${TB}{5} tn${tickn}`, F < B ? 'F<B' : '')
 
@@ -80,13 +79,13 @@ function GearI({
 		}
 		// 画基圆
 		function $B(style) {
-			$$({ color: '#999', dash: [1, 3], ...style })
+			$$({ color: '#ccc', ...style })
 			$.arc(x, y, B * zoom, 0, PI2), $$$()
 		}
-		// 画中分圆
-		function $P(style) {
-			$$({ color: '#ccc', ...style })
-			$.arc(x, y, P * zoom, 0, PI2), $$$()
+		// 画根圆
+		function $F(G, style) {
+			$$({ color: '#999', dash: [1, 3], ...style })
+			$.arc(x, y, F * zoom, 0, PI2), $$$()
 		}
 		// 画齿
 		function $C(T, z = 0, style, f = true) {
@@ -109,6 +108,17 @@ function GearI({
 		function $CZ(T, style) {
 			for (let z = 0; z < Z; z++) $C(T, z, style)
 		}
-		return Object.assign({ param: $param, x, y, O: $O, B: $B, P: $P, C: $C, CZ: $CZ })
+		return Object.assign({ param: $param, x, y, O: $O, B: $B, F: $F, C: $C, CZ: $CZ })
 	}
+}
+GearInv.T = A => tan(A) - A // 压力角求展角（展角+压力角=渐开角）
+
+GearInv.EEmin = (A, ZZ) => ((cos((A * PI) / 180) - 1) * ZZ) / 2 // 最小变距系数
+GearInv.E = (M, ZZ, EE) => M * (ZZ / 2 + EE) // 变距系数 求 中心距
+// 变距系数 求 变位系数和差
+GearInv.EESS = function (A, ZZ, EE) {
+	A = (A * PI) / 180
+	ZZ /= 2
+	let AW = acos(cos(A) / (EE / ZZ + 1))
+	return ((GearInv.T(AW) - GearInv.T(A)) / tan(A)) * ZZ
 }
